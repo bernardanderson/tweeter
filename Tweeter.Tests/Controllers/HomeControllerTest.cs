f381412a9996bb6d41fbb45b2a194171de017b89
Bernard Anderson <bernardanderson@gmail.com>
2016-11-12 11:18:19 -0600

    Working on Moq and unit tests

--- Tweeter.Tests/Controllers/HomeControllerTest.cs
+++ Tweeter.Tests/Controllers/HomeControllerTest.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Entity;
+using Moq;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tweeter;
+using Tweeter.DAL;
+using Tweeter.Models;
 using Tweeter.Controllers;
 
 namespace Tweeter.Tests.Controllers
@@ -12,6 +16,12 @@ namespace Tweeter.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
+        Mock<TweeterContext> mock_context { get; set; }
+        Mock<DbSet<Tweet>> mock_tweet_table { get; set; }
+        Mock<DbSet<Twit>> mock_twit_table { get; set; }
+        AccountController repo { get; set; }
+
+
         [TestMethod]
         public void Index()
         {
