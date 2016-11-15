d43e554848be02e44d15c3bd566c164f618ae2cb
Bernard Anderson <bernardanderson@gmail.com>
2016-11-14 20:22:44 -0600

    Partial completion of inclass

---
+++ Tweeter/app/app-controller.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+app.controller("userNameChecker", function($scope, $http) {
+
+    $scope.tester = "This is a test";
+
+    $scope.checkTheUserName = function() {
+
+        $http({
+            method: 'GET',
+            url: '/api/TwitUsername'
+        }).then(function successCallback(response) {
+            console.log(response);
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+
+        //$('#reporter').text("This has changed")
+    }
+});
